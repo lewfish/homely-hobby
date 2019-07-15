acdfa7601d403f13e11743d574e13a6687968404
Lewis Fishgold <lewfish@gmail.com>
2019-07-15 14:31:15 -0400

    Encode label array

--- mlx/od/fcos/encoder.py
+++ mlx/od/fcos/encoder.py
@@ -1,7 +1,9 @@
 import torch
 
-def encode_box(arr, stride, box):
-    # arr is (4, h, w) where first dimension ranges over t, l, b, r
+def encode_box(reg_arr, label_arr, stride, box, label):
+    # reg_arr is (4, h, w) where first dimension ranges over t, l, b, r
+    # ie. (top, left, bottom, right)
+    # class_arr is (c, h, w)
 
     # height and width of box have to be at least stride, or else it might
     # fall in between the center of two positions, and not be encodable.
@@ -9,7 +11,7 @@ def encode_box(arr, stride, box):
     if torch.min(box[2:] - box[0:2]) < stride:
         return
 
-    h, w = torch.Tensor(list(arr.shape[1:])) * stride
+    h, w = torch.Tensor(list(reg_arr.shape[1:])) * stride
     half_stride = torch.Tensor([stride//2, stride//2]).int()
     nw_ij = (box[0:2] + half_stride) // stride
     se_ij = (box[2:] - half_stride) // stride
@@ -29,4 +31,6 @@ def encode_box(arr, stride, box):
     box_arr[3, :, :] -= col_incs[None, :]
 
     nw_ij, se_ij = nw_ij.tolist(), se_ij.tolist()
-    arr[:, nw_ij[0]:se_ij[0]+1, nw_ij[1]:se_ij[1]+1] = box_arr
+    reg_arr[:, nw_ij[0]:se_ij[0]+1, nw_ij[1]:se_ij[1]+1] = box_arr
+
+    label_arr[label, nw_ij[0]:se_ij[0]+1, nw_ij[1]:se_ij[1]+1] = 1
--- mlx/od/fcos/test_encoder.py
+++ mlx/od/fcos/test_encoder.py
@@ -6,43 +6,67 @@ from mlx.od.fcos.encoder import encode_box
 
 class TestEncoder(unittest.TestCase):
     def test_encode_too_small(self):
-        arr = torch.zeros((4, 3, 3))
+        num_labels = 3
+        label_arr = torch.zeros((num_labels, 3, 3))
+        label = 1
+        reg_arr = torch.zeros((4, 3, 3))
         stride = 4
         box = torch.Tensor([0, 0, 2, 2])
-        encode_box(arr, stride, box)
-        exp_arr = torch.zeros((4, 3, 3))
-        self.assertTrue(arr.equal(exp_arr))
+        encode_box(reg_arr, label_arr, stride, box, label)
+
+        exp_reg_arr = torch.zeros((4, 3, 3))
+        self.assertTrue(reg_arr.equal(exp_reg_arr))
 
     def test_encode1(self):
-        arr = torch.zeros((4, 3, 3))
+        num_labels = 3
+        label_arr = torch.zeros((num_labels, 3, 3))
+        label = 1
+        reg_arr = torch.zeros((4, 3, 3))
         stride = 4
         box = torch.Tensor([3, 3, 9, 9])
-        encode_box(arr, stride, box)
-        exp_arr = torch.zeros((4, 3, 3))
-        exp_arr[:, 1, 1] = torch.Tensor([3, 3, 3, 3])
-        self.assertTrue(arr.equal(exp_arr))
+        encode_box(reg_arr, label_arr, stride, box, label)
+
+        exp_reg_arr = torch.zeros((4, 3, 3))
+        exp_reg_arr[:, 1, 1] = torch.Tensor([3, 3, 3, 3])
+        self.assertTrue(reg_arr.equal(exp_reg_arr))
 
     def test_encode2(self):
-        arr = torch.zeros((4, 3, 3))
+        num_labels = 3
+        label_arr = torch.zeros((num_labels, 3, 3))
+        label = 1
+        reg_arr = torch.zeros((4, 3, 3))
         stride = 4
         box = torch.Tensor([0, 0, 4, 12])
-        encode_box(arr, stride, box)
-        exp_arr = torch.zeros((4, 3, 3))
-        exp_arr[:, 0, 0] = torch.Tensor([2, 2, 2, 10])
-        exp_arr[:, 0, 1] = torch.Tensor([2, 6, 2, 6])
-        exp_arr[:, 0, 2] = torch.Tensor([2, 10, 2, 2])
-        self.assertTrue(arr.equal(exp_arr))
+        encode_box(reg_arr, label_arr, stride, box, label)
+
+        exp_reg_arr = torch.zeros((4, 3, 3))
+        exp_reg_arr[:, 0, 0] = torch.Tensor([2, 2, 2, 10])
+        exp_reg_arr[:, 0, 1] = torch.Tensor([2, 6, 2, 6])
+        exp_reg_arr[:, 0, 2] = torch.Tensor([2, 10, 2, 2])
+        self.assertTrue(reg_arr.equal(exp_reg_arr))
+
+        exp_label_arr = torch.zeros((num_labels, 3, 3))
+        exp_label_arr[label, 0, :] = 1
+        self.assertTrue(label_arr.equal(exp_label_arr))
 
     def test_encode3(self):
-        arr = torch.zeros((4, 3, 3))
+        num_labels = 3
+        label_arr = torch.zeros((num_labels, 3, 3))
+        label = 1
+        reg_arr = torch.zeros((4, 3, 3))
         stride = 4
         box = torch.Tensor([0, 8, 12, 12])
-        encode_box(arr, stride, box)
-        exp_arr = torch.zeros((4, 3, 3))
-        exp_arr[:, 0, 2] = torch.Tensor([2, 2, 10, 2])
-        exp_arr[:, 1, 2] = torch.Tensor([6, 2, 6, 2])
-        exp_arr[:, 2, 2] = torch.Tensor([10, 2, 2, 2])
-        self.assertTrue(arr.equal(exp_arr))
+        encode_box(reg_arr, label_arr, stride, box, label)
+
+        exp_reg_arr = torch.zeros((4, 3, 3))
+        exp_reg_arr[:, 0, 2] = torch.Tensor([2, 2, 10, 2])
+        exp_reg_arr[:, 1, 2] = torch.Tensor([6, 2, 6, 2])
+        exp_reg_arr[:, 2, 2] = torch.Tensor([10, 2, 2, 2])
+        self.assertTrue(reg_arr.equal(exp_reg_arr))
+
+        exp_label_arr = torch.zeros((num_labels, 3, 3))
+        exp_label_arr[label, :, 2] = 1
+        self.assertTrue(label_arr.equal(exp_label_arr))
 
 if __name__ == '__main__':
     unittest.main()
