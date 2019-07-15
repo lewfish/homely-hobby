e2de35fefa4df473343952950c85b2936dcd7d8c
Lewis Fishgold <lewfish@gmail.com>
2019-07-15 17:23:27 -0400

    Encode multiple boxes and labels onto multi-scale pyramid

--- mlx/od/fcos/test_encoder.py
+++ mlx/od/fcos/test_encoder.py
@@ -2,9 +2,9 @@ import unittest
 
 import torch
 
-from mlx.od.fcos.encoder import encode_box
+from mlx.od.fcos.encoder import encode_target, encode_targets
 
-class TestEncoder(unittest.TestCase):
+class TestEncodeTarget(unittest.TestCase):
     def test_encode_too_small(self):
         num_labels = 3
         label_arr = torch.zeros((num_labels, 3, 3))
@@ -12,7 +12,7 @@ class TestEncoder(unittest.TestCase):
         reg_arr = torch.zeros((4, 3, 3))
         stride = 4
         box = torch.Tensor([0, 0, 2, 2])
-        encode_box(reg_arr, label_arr, stride, box, label)
+        encode_target(reg_arr, label_arr, stride, box, label)
 
         exp_reg_arr = torch.zeros((4, 3, 3))
         self.assertTrue(reg_arr.equal(exp_reg_arr))
@@ -24,7 +24,7 @@ class TestEncoder(unittest.TestCase):
         reg_arr = torch.zeros((4, 3, 3))
         stride = 4
         box = torch.Tensor([3, 3, 9, 9])
-        encode_box(reg_arr, label_arr, stride, box, label)
+        encode_target(reg_arr, label_arr, stride, box, label)
 
         exp_reg_arr = torch.zeros((4, 3, 3))
         exp_reg_arr[:, 1, 1] = torch.Tensor([3, 3, 3, 3])
@@ -37,7 +37,7 @@ class TestEncoder(unittest.TestCase):
         reg_arr = torch.zeros((4, 3, 3))
         stride = 4
         box = torch.Tensor([0, 0, 4, 12])
-        encode_box(reg_arr, label_arr, stride, box, label)
+        encode_target(reg_arr, label_arr, stride, box, label)
 
         exp_reg_arr = torch.zeros((4, 3, 3))
         exp_reg_arr[:, 0, 0] = torch.Tensor([2, 2, 2, 10])
@@ -56,7 +56,7 @@ class TestEncoder(unittest.TestCase):
         reg_arr = torch.zeros((4, 3, 3))
         stride = 4
         box = torch.Tensor([0, 8, 12, 12])
-        encode_box(reg_arr, label_arr, stride, box, label)
+        encode_target(reg_arr, label_arr, stride, box, label)
 
         exp_reg_arr = torch.zeros((4, 3, 3))
         exp_reg_arr[:, 0, 2] = torch.Tensor([2, 2, 10, 2])
@@ -67,6 +67,43 @@ class TestEncoder(unittest.TestCase):
         exp_label_arr = torch.zeros((num_labels, 3, 3))
         exp_label_arr[label, :, 2] = 1
         self.assertTrue(label_arr.equal(exp_label_arr))
+
+class TestEncodeTargets(unittest.TestCase):
+    def test_encode(self):
+        pyramid_shape = [
+            (8, 8, 8, 8),
+            (16, 16, 4, 4),
+            (32, 32, 2, 2)
+        ]
+        num_labels = 2
+        boxes = torch.Tensor([
+            [0, 0, 16, 16],
+            [16, 16, 32, 32],
+            [0, 0, 32, 32]
+        ])
+        labels = torch.Tensor([0, 0, 1])
+        targets = encode_targets(boxes, labels, pyramid_shape, num_labels)
+
+        self.assertTrue(torch.all(targets[8]['reg_arr'] == 0))
+        self.assertTrue(torch.all(targets[8]['label_arr'] == 0))
+        self.assertTrue(targets[8]['reg_arr'].shape == (4, 8, 8))
+        self.assertTrue(targets[8]['label_arr'].shape == (2, 8, 8))
+
+        exp_reg_arr = torch.zeros((4, 4, 4))
+        exp_reg_arr[:, 0, 0] = torch.Tensor([8, 8, 8, 8])
+        exp_reg_arr[:, 1, 1] = torch.Tensor([8, 8, 8, 8])
+        self.assertTrue(targets[16]['reg_arr'].equal(exp_reg_arr))
+        exp_label_arr = torch.zeros((2, 4, 4))
+        exp_label_arr[0, 0, 0] = 1
+        exp_label_arr[0, 1, 1] = 1
+        self.assertTrue(targets[16]['label_arr'].equal(exp_label_arr))
+
+        exp_reg_arr = torch.zeros((4, 2, 2))
+        exp_reg_arr[:, 0, 0] = torch.Tensor([16, 16, 16, 16])
+        self.assertTrue(targets[32]['reg_arr'].equal(exp_reg_arr))
+        exp_label_arr = torch.zeros((2, 2, 2))
+        exp_label_arr[1, 0, 0] = 1
+        self.assertTrue(targets[32]['label_arr'].equal(exp_label_arr))
 
 if __name__ == '__main__':
     unittest.main()
