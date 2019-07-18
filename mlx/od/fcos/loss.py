ff7b58638f3c2fc95a4b1cb26d5603ced69cea28
Lewis Fishgold <lewfish@gmail.com>
2019-07-18 18:15:47 -0400

    Add focal loss

---
+++ mlx/od/fcos/loss.py
@@ -0,0 +1,10 @@
+import torch
+
+def focal_loss(output, target, gamma=2, alpha=0.25):
+    p = torch.sigmoid(output)
+    pt = (1-target) * (1-p) + target * p
+    alphat = (1-target) * (1-alpha) + target * alpha
+    bce = -torch.log(pt)
+    weights = alphat * (1 - pt).pow(gamma)
+    loss_arr = weights * bce
+    return loss_arr.reshape(-1).sum()
---
+++ mlx/od/fcos/test_loss.py
@@ -0,0 +1,27 @@
+import unittest
+
+import torch
+
+from mlx.od.fcos.loss import focal_loss
+
+class TestFocalLoss(unittest.TestCase):
+    def test_focal_loss(self):
+        gamma = 2
+        alpha = 0.25
+        p = torch.tensor([0.2, 0.7])
+        # do inverse of sigmoid
+        output = torch.log(p / (1-p))
+        target = torch.tensor([0.0, 1.0])
+
+        pt = torch.tensor([0.8, 0.7])
+        bce = -torch.log(pt)
+        alphat = torch.tensor([0.75, 0.25])
+        weights = torch.pow((1 - pt), gamma) * alphat
+        loss_arr = bce * weights
+        exp_loss = loss_arr.reshape(-1).sum()
+
+        loss = focal_loss(output, target, gamma, alpha)
+        self.assertEqual(exp_loss, loss)
+
+if __name__ == '__main__':
+    unittest.main()
