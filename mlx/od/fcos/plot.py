9c78cbaffe2e04ff94ab881c5f3195ebfedd4b78
Lewis Fishgold <lewfish@gmail.com>
2019-07-23 13:14:20 -0400

    Only show boxes with high enough score

--- mlx/od/fcos/plot.py
+++ mlx/od/fcos/plot.py
@@ -12,7 +12,7 @@ import torch
 from mlx.filesystem.utils import make_dir, zipdir
 
 
-def plot_preds(data, model, classes, output_dir, max_plots=50):
+def plot_preds(data, model, classes, output_dir, max_plots=50, score_thresh=0.4):
     preds_dir = join(output_dir, 'preds')
     zip_path = join(output_dir, 'preds.zip')
     make_dir(preds_dir, force_empty=True)
@@ -29,8 +29,13 @@ def plot_preds(data, model, classes, output_dir, max_plots=50):
         # Plot predictions
         h, w = x.shape[1:]
         z = z[0]
+        # Filter boxes whose score is high enough
         boxes = z['boxes'].cpu()
         labels = z['labels'].cpu()
+        scores = z['scores'].cpu()
+        keep_inds = scores > score_thresh
+        boxes, labels, scores = boxes[keep_inds, :], labels[keep_inds], scores[keep_inds]
+
         if z['boxes'].shape[0] > 0:
             z = ImageBBox.create(h, w, boxes, labels, classes=classes,
                                  scale=True)
@@ -46,9 +51,9 @@ def plot_preds(data, model, classes, output_dir, max_plots=50):
         img.save(join(preds_dir, '{}-orig.png'.format(img_id)))
 
         # Plot raw output of network
-        for s, out in head_out.items():
+        for stride, level_out in head_out.items():
             # Plot probs for each label
-            label_arr = out['label_arr'][0].detach().cpu()
+            label_arr = level_out['label_arr'][0].detach().cpu()
             label_probs = torch.sigmoid(label_arr).numpy()
 
             plt.gca()
@@ -59,13 +64,13 @@ def plot_preds(data, model, classes, output_dir, max_plots=50):
                 a = label_probs[l]
                 plt.imshow(a)
 
-            plt.suptitle('label probs for stride={}'.format(s))
+            plt.suptitle('label probs for stride={}'.format(stride))
             plt.savefig(
-                join(preds_dir, '{}-{}-label-arr.png'.format(img_id, s)),
+                join(preds_dir, '{}-{}-label-arr.png'.format(img_id, stride)),
                 figsize=(3, 3))
 
             # Plot top, left, bottom, right arrays.
-            reg_arr = out['reg_arr'][0].detach().cpu().numpy()
+            reg_arr = level_out['reg_arr'][0].detach().cpu().numpy()
             plt.gca()
             directions = ['top', 'left', 'bottom', 'right']
             for di, d in enumerate(directions):
@@ -74,9 +79,9 @@ def plot_preds(data, model, classes, output_dir, max_plots=50):
                 a = reg_arr[di]
                 plt.imshow(a)
 
-            plt.suptitle('distance to box edge for stride={}'.format(s))
+            plt.suptitle('distance to box edge for stride={}'.format(stride))
             plt.savefig(
-                join(preds_dir, '{}-{}-reg-arr.png'.format(img_id, s)),
+                join(preds_dir, '{}-{}-reg-arr.png'.format(img_id, stride)),
                 figsize=(10, 10))
 
     zipdir(preds_dir, zip_path)
