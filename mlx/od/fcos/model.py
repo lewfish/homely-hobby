9443da3096861f5b78e585873059db960db0394c
Lewis Fishgold <lewfish@gmail.com>
2019-08-08 16:17:09 -0400

    Use group norm instead of batch norm in head

--- mlx/od/fcos/model.py
+++ mlx/od/fcos/model.py
@@ -114,11 +114,11 @@ class ConvBlock(nn.Module):
         super().__init__()
         self.conv = nn.Conv2d(in_channels, out_channels, kernel_size,
                               padding=padding)
-        self.bn = nn.BatchNorm2d(out_channels)
+        self.gn = nn.GroupNorm(32, out_channels)
 
     def forward(self, x):
         x = self.conv(x)
-        x = self.bn(x)
+        x = self.gn(x)
         x = nn.functional.relu(x)
         return x
 
