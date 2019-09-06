253c1457c8ab6541255250575396d4f7ddd8d743
Lewis Fishgold <lewfish@gmail.com>
2019-09-06 11:18:31 -0400

    wip

--- scripts/create_spot_request.py
+++ scripts/create_spot_request.py
@@ -5,44 +5,47 @@ import datetime
 
 client = boto3.client('ec2', region_name='us-east-1')
 
-'''
-response = client.request_spot_instances(
+response = client.request_spot_fleet(
     DryRun=False,
-    SpotPrice='0.5',
-    InstanceCount=1,
-    Type='one-time',
-    LaunchSpecification={
-        'ImageId': 'ami-0241ac1f637a90b84',
-        'KeyName': 'raster-vision-team',
-        'InstanceType': 'p2.xlarge',
-        'Placement': {
-            'AvailabilityZone': 'us-east-1a',
-        },
-        'SecurityGroupIds': [
-            'sg-06984b3d26ba2115e'
-        ],
-        'BlockDeviceMappings': [
+    SpotFleetRequestConfig={
+        'IamFleetRole': 'arn:aws:iam::279682201306:role/lfishgoldRasterVisionSpotFleetRole',
+        'LaunchSpecifications': [
             {
-                'DeviceName': '/dev/xvda',
-                'Ebs': {
-                    'VolumeSize': 100,
-                    'DeleteOnTermination': True,
-                    'VolumeType': 'gp2',
-                    'Encrypted': False
+                'ImageId': 'ami-0241ac1f637a90b84',
+                'KeyName': 'raster-vision-team',
+                'InstanceType': 'p2.xlarge',
+                'Placement': {
+                    'AvailabilityZone': 'us-east-1a',
                 },
-            },
+                'SecurityGroupIds': [
+                    'sg-06984b3d26ba2115e'
+                ],
+                'BlockDeviceMappings': [
+                    {
+                        'DeviceName': '/dev/xvda',
+                        'Ebs': {
+                            'VolumeSize': 100,
+                            'DeleteOnTermination': True,
+                            'VolumeType': 'gp2',
+                            'Encrypted': False
+                        },
+                    },
+                ],
+                'EbsOptimized': False,
+                'Monitoring': {
+                    'Enabled': True
+                },
+
+            }
         ],
-        'EbsOptimized': False,
-        'Monitoring': {
-            'Enabled': True
-        },
+
     }
+    SpotPrice='0.5',
+    TargetCapacity=1,
+    Type='request'
 )
 
 spot_instance_request_id = response['SpotInstanceRequests'][0]['SpotInstanceRequestId']
-'''
-
-spot_instance_request_id = 'sir-arn8533j'
 
 print('Waiting for spot request to be fulfilled...')
 waiter = client.get_waiter('spot_instance_request_fulfilled')
